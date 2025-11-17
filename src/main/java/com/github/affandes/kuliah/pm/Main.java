3046efc2918877183c89bba415b2bc62220e0bc1
hafizh1406 <12450113061@students.uin-suska.ac.id>
2025-11-17 18:10:22 +0700

    Update Main.java

--- src/main/java/com/github/affandes/kuliah/pm/Main.java
+++ src/main/java/com/github/affandes/kuliah/pm/Main.java
@@ -1,26 +1,26 @@
 package com.github.affandes.kuliah.pm;
 
-public class Main {
-    public static void main(String[] args) {
-        // Tulis kode disini
-        import java.util.Stack;
+
+import java.util.Stack;
 import java.util.Scanner;
 
-public class BrowserHistory {
+public class broser {
     public static void main(String[] args) {
-
         Stack<String> history = new Stack<>();
         Scanner input = new Scanner(System.in);
+        
         history.push("google.com");
         history.push("uin-suska.ac.id");
+
         System.out.println("(Simulasi: 2 data history telah ditambahkan)");
         while (true) {
-            System.out.println("\n >>> Menu Browser <<<");
-            System.out.println("1. browse ");
-            System.out.println("2. back 3");
-            System.out.println("3. view ");
-            System.out.println("4. exit ");
+            System.out.println("\n===== Menu Browser =====");
+            System.out.println("1. Browse");
+            System.out.println("2. Back");
+            System.out.println("3. View History");
+            System.out.println("4. Exit");
             System.out.print("Masukkan pilihan Anda (1-4): ");
+
             int pilihan;
 
             try {
@@ -29,7 +29,6 @@ public class BrowserHistory {
                 System.out.println("Input tidak valid. Harap masukkan angka.");
                 continue;
             }
-
             if (pilihan == 1) {
                 System.out.print("Masukkan nama website baru: ");
                 String websiteBaru = input.nextLine();
@@ -37,7 +36,6 @@ public class BrowserHistory {
                 System.out.println("Berhasil mengunjungi: " + websiteBaru);
 
             } else if (pilihan == 2) {
-
                 if (history.isEmpty()) {
                     System.out.println("History kosong, tidak dapat kembali.");
                 } else {
@@ -50,7 +48,6 @@ public class BrowserHistory {
                         System.out.println("History saat ini kosong.");
                     }
                 }
-
             } else if (pilihan == 3) {
                 System.out.println("\n--- Riwayat Browser (Terbaru) ---");
 
@@ -61,16 +58,15 @@ public class BrowserHistory {
                         System.out.println(history.get(i));
                     }
                 }
+
             } else if (pilihan == 4) {
                 System.out.println("Program dihentikan.");
                 break;
+
             } else {
                 System.out.println("Pilihan tidak valid. Harap masukkan angka 1-4.");
             }
         }
         input.close();
-    }
-}
-
     }
 }
